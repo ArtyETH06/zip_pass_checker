6db4a8fde01d615ef7ddc4b18e75ece6b892ddad
Arthur RAILLON <107058122+ArtyETH06@users.noreply.github.com>
2023-02-13 11:32:47 +0100

    Add files via upload

---
+++ zip_pass_checker.py
@@ -0,0 +1,32 @@
+#Imports
+import zipfile
+import argparse
+
+#Création de la fonction
+def has_password(zip_file):
+    try:
+        with zipfile.ZipFile(zip_file, 'r') as zf:
+            zf.readall()
+            return False
+    except:
+        return True
+
+
+#Parser creation
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Check si un fichier ZIP contient un mot de passe")
+    parser.add_argument("-f", type=str, help="Sélectionne le fichier à vérifier si il contient un mot de passe.")
+    args = parser.parse_args()
+
+print(args.f)
+
+'''
+zip_file = 'to_crack.zip'
+
+#test + Réponses
+if has_password(zip_file):
+    print(f'{zip_file} is password protected.')
+else:
+    print(f'{zip_file} is not password protected.')
+'''
